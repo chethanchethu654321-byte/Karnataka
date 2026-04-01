a472fa7773703065f2149d6c7b1fc4d8dbe794ab
Chethan <chethanchethu654321@gmail.com>
2026-04-01 21:17:00 +0530

    Initial commit - Karnataka premium website

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById("image1").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Bengaluru")
+
+}
+document.getElementById("image2").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Mysore")
+
+}
+document.getElementById("image3").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Hampi")
+
+}
+document.getElementById("image4").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Coorg")
+
+}
+document.getElementById("image5").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Gokarna")
+
+}
+document.getElementById("image6").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Jogfalls")
+
+}
+document.getElementById("image7").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Davanagere")
+
+}
+document.getElementById("image8").onclick=function(){
+    window.open("https://en.wikipedia.org/wiki/Chikkamagaluru")
+
+}
+
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Message Sent Successfully 🚀");
+});
